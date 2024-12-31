f8921c5b5d93fe83984160a03ebf7ac490f445e4
ABUBAKAR-SIDDIQUE2 <a3325452033@gmail.com>
2024-12-31 18:28:21 +0500

    Assignment#7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,46 @@
-import Image from "next/image";
+import React from 'react'
+// import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface Product{
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: {
+    rate: number,
+    count: number
+  }
 }
+
+async function Home() {
+
+  const res = await fetch('https://fakestoreapi.com/products')
+  const products: Product[] = await res.json()
+
+  return (
+    <div>
+
+      <h1 className='flex justify-center pt-3 text-8xl text-[#ff7d1a] font-semibold'>FASHION STORE</h1>
+
+      <div className='w-full h-full pl-40 p-16 grid grid-cols-3'>
+        {products.map(product => (
+          <div key={product.id} className='flex flex-col items-center w-[280px] h-[440px] mb-16 p-4 border border-[#ff7d1a] rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-out hover:shadow-lg hover:shadow-[#ff7d1a]'>
+            <img src={product.image} alt="Product Image" className='w-[230px] h-[280px] rounded-2xl shadow-md'/>
+            <p className='text-[#ff7d1a] uppercase text-[12px] font-bold mr-auto mt-4'>{product.category}</p>
+            <h2 className='w-[250px] h-[30px] text-xl font-semibold mr-auto mt-1 overflow-clip'>{product.title}</h2>
+            <p className='text-2xl font-bold text-[#1d2025] mr-auto mt-1'>${product.price}</p>
+            <p className='mr-auto font-medium mt-1'>
+              <span className='text-yellow-500'>★</span>
+              {product.rating.rate} ({product.rating.count})
+            </p>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
